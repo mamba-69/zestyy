046de9915bb5fc37535b38766685885869fcb2c3
mamba-69 <jennypankajpatel0511@gmail.com>
2025-07-27 19:31:25 +0530

    jghcf

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['iili.io'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'iili.io',
+        pathname: '**',
+      },
+    ],
   },
 };
 
